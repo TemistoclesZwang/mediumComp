84370df1389321c8f5ec720386b5a2829b3a8912
Temistocles Zwang <temis2st@gmail.com>
2021-09-24 11:13:46 -0300

    apagado os comentarios de teste

--- MonteCarlo problem/monterCarloREFATORA.py
+++ MonteCarlo problem/monterCarloREFATORA.py
@@ -2,13 +2,12 @@ import pandas as pd
 # ;LIMPAR COMENTARIOS NA VERSÃO FINAL
 
 array_aprox = []
-capacidadeMax  = 3 #120 MC15 #.eixo Y número 1
+capacidadeMax  = 3 #.eixo Y número 1
 passagensOverbooking = 2 #quantas passagens a mais quero verificar
-calcRangePassagensaMais = capacidadeMax + passagensOverbooking#-1 pois o range começa em 0 no eixo Y da primeira colulna
+calcRangePassagensaMais = capacidadeMax + passagensOverbooking
 
 def bernoulli_0_ou_1(qtdPassagens):
    from random import random
-   # qtdPassagens = int(input())
    n = [0.0] * qtdPassagens #para gerar X números flutuantes
 
    for i in range(qtdPassagens):
@@ -25,14 +24,6 @@ def gerarSimulacoesBernoulli(df):
    for i in range(1, qtdSimulacoes+1): #SEMPRE UMA A MAIS DEVIDO A COLUNA A MENOS GERADA PELO PANDAS df
       df.insert(i, f"U_{i}", bernoulli_0_ou_1(qtdPassagens), True)
 
-
-# def no_Show(totalPassagensVendidas):
-#    if totalPassagensVendidas == calcRangePassagensaMais: 
-#       return 0 #se ninguém faltou não tem no show
-#    elif totalPassagensVendidas < calcRangePassagensaMais:
-#       return (totalPassagensVendidas - capacidadeMax) #Esse 1 é do index do pandas
-#    else:
-      # return 0
 
 def no_Show(totalPassagensVendidas,qtdPassagens): 
    if totalPassagensVendidas == calcRangePassagensaMais: 
@@ -74,12 +65,13 @@ def gerarInformacoesIndividuais(i,totalPassagensVendidas):
    print (f"Remarcação: {remarcacao(totalPassagensVendidas)}")
    print (f"Arrecadado: {arrecadado(totalPassagensVendidas)}")
    print('\n')
-   
+
 
 def somarPassagens(df): #.eixo Y
    media_aproximacao = 0
    for i in range(1, qtdSimulacoes+1):
       Total = df[f"U_{i}"].sum() #total de cada coluna
+   # .modo basico que so mostra resultado final (para computadores simples)
       # no_Show(Total)
       # overbooking(Total)
       # multa(Total)
@@ -97,16 +89,16 @@ def somarPassagens(df): #.eixo Y
    return media_aproximacao/(qtdSimulacoes)
 
 
-for vendasaMais in range (capacidadeMax+1, calcRangePassagensaMais+1): #+1 pois quero que conte a partir da primeira passagem vendida a mais
+#+1 pois quero que conte a partir da primeira passagem vendida 
+# a mais e não conte dos casos que não vendeu a mais
+for vendasaMais in range (capacidadeMax+1, calcRangePassagensaMais+1): 
    qtdPassagens = vendasaMais #.eixo X de passageiros
    qtdSimulacoes = 3 #.eixo Y de simulacoes (U) 
    data = {'': [' '] * qtdPassagens}
    df = pd.DataFrame(data)# converter o dicionário em dataframe
    gerarSimulacoesBernoulli(df)
-   # somarPassagens(df) #,teste
    array_aprox.append(somarPassagens(df))
    print (f"### vendas a mais: {(vendasaMais - capacidadeMax)+1}")
-   # print (f"### vendas a mais: {vendasaMais} \n### aproximacao: {somarPassagens(df)}")
    print(df) #,gerar tabela grafica no terminal
 
 
